890b7ff7b5dc841816a62a80b1bd348e02c45894
Oriva <weelman93@gmail.com>
2020-07-30 16:07:20 +0300

    save

--- js/calc.js
+++ js/calc.js
@@ -8,14 +8,12 @@ const cycleRange = ((num) => {
         elem50.get(z).classList.remove('advert-ability_active');
     }
 });
-const getPriceDev = (()=>{
-    let activeCompany = $('.advert-checkbox input:checked').length;
+const getPriceDev = (() => {
+    let activeCompany = $('.advert-checkbox input[data-dev]:checked').length;
     let countCompany = $('.advert-collection').length;
     let devResult = Math.round((1 - (activeCompany-1)*0.1)*activeCompany*countCompany*25000);
+
     $('.js-price-dev').html(devResult + ' ₽');
-});
-$('.advert-checkbox').on('click', ()=>{
-    getPriceDev();
 });
 $(".js-range-slider").ionRangeSlider({
     from: 2,
@@ -47,14 +45,74 @@ $(".js-range-slider").ionRangeSlider({
     onChange: function (data) {
         $('.advert-price').removeClass('advert-price_infinity');
         if (typeof data.from_value === 'number') {
-            cycleRange((data.from_value-50)/50);
+            cycleRange((data.from_value - 50) / 50);
             $('.advert-graph__price').html(data.from_value + ' 000 ₽');
-        } else if(data.from_value==='1 млн') {
+        } else if (data.from_value === '1 млн') {
             elem50.addClass('advert-ability_active');
             $('.advert-graph__price').html('1 000 000 ₽');
         } else {
             $('.advert-price').addClass('advert-price_infinity');
             $('.advert-graph__price').html('Не ограничен');
         }
+    }
+});
+
+let numElem = 1;
+$('#advert-add-theme').on('click', () => {
+    numElem++;
+    let collectElem = document.createElement('div');
+    collectElem.classList.add('advert-collection');
+    collectElem.innerHTML = '' +
+        '<div class="row align-items-center">' +
+        '<div class="advert-calc__text-col vam-child">' +
+        '<span class="advert-calc__title">Название тематики</span>' +
+        '<div class="get-info" data-info-id=""></div>' +
+        '</div>' +
+        '<div class="advert-theme-block">' +
+        '<div class="input-group advert-theme-block__input">' +
+        '<input type="text" class="input-group__input" name="name" autocomplete="off" placeholder="-"' +
+        '   required>' +
+        '<span class="bar"></span>' +
+        '<label>Введите название</label>' +
+        '</div>' +
+        '</div>' +
+        '<div class="advert-delete-block">' +
+        '<span class="advert-delete-js">- Убрать</span>' +
+        '</div>' +
+        '</div>' +
+        '<div class="row align-items-center advert-calc__second-block">' +
+        '<div class="advert-calc__text-col vam-child">' +
+        '<span class="advert-calc__title">Рекламная система</span>' +
+        '<div class="get-info" data-info-id=""></div>' +
+        '</div>' +
+        '<div class="advert-calc__checkbox">' +
+        '<div class="advert-checkbox">' +
+        '<input type="checkbox" name="advert-system" value="Яндекс.Директ" id="yd_' + numElem + '" data-dev="25000">' +
+        '<label for="yd_' + numElem + '">Яндекс.Директ</label>' +
+        '</div>' +
+        '<div class="advert-checkbox">' +
+        '<input type="checkbox" name="advert-system" value="Google Ads" id="google_' + numElem + '" data-dev="25000">' +
+        '<label for="google_' + numElem + '">Google Ads</label>' +
+        '</div>' +
+        '<div class="advert-checkbox">' +
+        '<input type="checkbox" name="advert-system" value="VK" id="vk_' + numElem + '">' +
+        '<label for="vk_' + numElem + '">VK</label>' +
+        '</div>' +
+        '<div class="advert-checkbox">' +
+        '<input type="checkbox" name="advert-system" value="Google Ads" id="fb_' + numElem + '">' +
+        '<label for="fb_' + numElem + '">Instagram + Facebook</label>' +
+        '</div>' +
+        '</div>' +
+        '</div>';
+    $('.advert-collections').append(collectElem)
+});
+
+$('.advert-calc').on('click', (e) => {
+    if (e.target.classList.contains('advert-delete-js')) {
+        numElem--;
+        e.target.closest('.advert-collections').removeChild(e.target.closest('.advert-collection'));
+    }
+    if (e.target.closest('.advert-checkbox')) {
+        getPriceDev();
     }
 });
