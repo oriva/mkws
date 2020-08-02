05bf09c80c1699d5cb101be29595fcf88c4ccaf1
Oriva <weelman93@gmail.com>
2020-08-02 23:09:58 +0300

    add popup

--- js/calc.js
+++ js/calc.js
@@ -1,12 +1,28 @@
 'use strict';
-const budget = 150000;
+let cities = [];
+$.getJSON('region.json', function(data) {
+    cities = data;
+    console.log(cities);
+});
+
+let numElem = 1;
+let budget = 0;
+let priceOptions = 0;
+const getPriceOptions = (()=>{
+    budget = 0;
+    $('.advert-ability:not(.advert-ability_active) input:checked').each((key, item)=>{
+        budget += item.closest('.advert-ability').dataset.price;
+    });
+});
 const elem50 = $('.advert-price').find('.advert-ability[data-need]');
 const cycleRange = ((num) => {
     for (let i = 0; i < num; i++) {
         elem50.get(i).classList.add('advert-ability_active');
+        elem50.get(i).querySelector('input').checked = true;
     }
     for (let z = num; z < elem50.length; z++) {
         elem50.get(z).classList.remove('advert-ability_active');
+        elem50.get(z).querySelector('input').checked = false;
     }
 });
 const getPriceDev = (() => {
@@ -17,9 +33,14 @@ const getPriceDev = (() => {
             countCompany++;
         }
     });
-    let devResult = Math.round((1 - (activeCompany-1)*0.1)*activeCompany*countCompany*25000);
-
+    let devResult = Math.round((1 - (activeCompany - 1) * 0.1) * activeCompany * countCompany * 25000);
     $('.js-price-dev').html(devResult + ' ₽');
+});
+const getPriceSev = (() => {
+    getPriceOptions();
+    let countAdvAll = $('.advert-collections').find('input:checked').length;
+    let serResult = Math.round((1 - (countAdvAll - 1) * 0.1) * countAdvAll * numElem * 20000 - 0.01 * budget + priceOptions * numElem) + ' ₽';
+    $('.js-price-serv').html(serResult);
 });
 $(".js-range-slider").ionRangeSlider({
     from: 2,
@@ -53,17 +74,20 @@ $(".js-range-slider").ionRangeSlider({
         if (typeof data.from_value === 'number') {
             cycleRange((data.from_value - 50) / 50);
             $('.advert-graph__price').html(data.from_value + ' 000 ₽');
+            if (data.from_value >= 150)
+                budget = data.from_value * 1000;
         } else if (data.from_value === '1 млн') {
             elem50.addClass('advert-ability_active');
+            $('.advert-price .advert-ability[data-need] input').prop('checked', true);
             $('.advert-graph__price').html('1 000 000 ₽');
+            budget = 1000000;
         } else {
             $('.advert-price').addClass('advert-price_infinity');
             $('.advert-graph__price').html('Не ограничен');
         }
+        getPriceSev();
     }
 });
-
-let numElem = 1;
 $('#advert-add-theme').on('click', () => {
     numElem++;
     let collectElem = document.createElement('div');
@@ -115,6 +139,7 @@ $('#advert-add-theme').on('click', () => {
         '</div>';
     $('.advert-collections').append(collectElem).addClass('can-delete');
     getPriceDev();
+    getPriceSev();
 });
 
 $('.advert-calc').on('click', (e) => {
@@ -125,7 +150,6 @@ $('.advert-calc').on('click', (e) => {
             $('.advert-collections').removeClass('can-delete');
         }
     }
-
     // минимум 1 инпут
     if(e.target.closest('.advert-checkbox')) {
         if(e.target.closest('.advert-calc__checkbox').querySelectorAll('input:checked').length===1 && e.target.closest('.advert-checkbox').querySelector('input').checked) {
@@ -133,6 +157,31 @@ $('.advert-calc').on('click', (e) => {
         } else {
             e.target.closest('.advert-checkbox').querySelector('input').checked = !e.target.closest('.advert-checkbox').querySelector('input').checked;
             getPriceDev();
+            getPriceSev();
+        }
+    }
+    if(e.target.tagName==='SPAN' && e.target.closest('.advert-popup__ul')) {
+        e.target.previousElementSibling.checked = !e.target.previousElementSibling.checked;
+        if(e.target.parentNode.classList.contains('advert-popup__parent')) {
+            $(e.target.parentNode.nextElementSibling.querySelectorAll('input')).prop('checked', e.target.previousElementSibling.checked);
+        }
+    }
+    if(e.target.tagName==='SPAN' && e.target.parentNode.classList.contains('advert-popup__parent') || e.target.classList.contains('advert-popup__arrow')) {
+        if(e.target.tagName==='SPAN') {
+            e.target.parentNode.classList.add('show');
+            $(e.target.parentNode.nextElementSibling).slideDown();
+        } else {
+            e.target.parentNode.classList.toggle('show');
+            $(e.target.parentNode.nextElementSibling).slideToggle();
+        }
+    }
+});
+
+$('.advert-price').on('click', (e)=>{
+    if(e.target.closest('.advert-ability') && e.target.closest('.advert-ability').querySelector('input')) {
+        if(e.target.closest('.advert-ability').dataset.need>50 && !e.target.closest('.advert-ability').classList.contains('advert-ability_active')) {
+            e.target.closest('.advert-ability').querySelector('input').checked = !e.target.closest('.advert-ability').querySelector('input').checked;
+            getPriceSev();
         }
     }
 });
